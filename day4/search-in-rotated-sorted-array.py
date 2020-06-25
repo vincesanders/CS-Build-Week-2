0fdc9094114142cfec14cf01e4642ed9bc859b7f
Vincent Sanders <vincesanders@hotmail.com>
2020-06-25 17:29:37 -0500

    Slight refactoring of search in rotated sorted array.

--- day4/search-in-rotated-sorted-array.py
+++ day4/search-in-rotated-sorted-array.py
@@ -2,7 +2,6 @@ from typing import List
 
 class Solution:
     def binary_recursive_search(self, nums: List[int], target: int, low=0) -> int:
-        found_at_index = None
         if len(nums) < 1:
             return None
         elif len(nums) is 1 and nums[0] != target:
@@ -10,16 +9,15 @@ class Solution:
         else:
             middle = (len(nums) - 1) // 2
             if target == nums[middle]:
-                found_at_index = low + middle
-                return found_at_index
+                return low + middle
             else:
-                found_at_index = self.binary_recursive_search(nums[:middle], target, low)
-                if found_at_index is None:
-                    found_at_index = self.binary_recursive_search(nums[middle + 1:], target, low + middle + 1)
+                index = self.binary_recursive_search(nums[:middle], target, low)
+                if index is None:
+                    index = self.binary_recursive_search(nums[middle + 1:], target, low + middle + 1)
                 else:
-                    return found_at_index
-                if found_at_index is not None:
-                    return found_at_index
+                    return index
+                if index is not None:
+                    return index
 
     def search(self, nums: List[int], target: int) -> int:
         found_at_index = self.binary_recursive_search(nums, target)
@@ -31,4 +29,4 @@ class Solution:
 array = [4,5,6,7,0,1,2]
 solution = Solution()
 
-print(solution.search(array, 3))
+print(solution.search(array, 2))
